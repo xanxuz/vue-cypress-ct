5a804e3a76be4fcd3d5400d519ff12d1fe5013f3
Leonardo <leonardosalazard@gmail.com>
2021-04-13 18:35:43 -0400

    chore: add vue pwa plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: [
     'plugin:vue/essential',
     '@vue/standard',
-    "plugin:cypress/recommended"
+    'plugin:cypress/recommended'
   ],
   parserOptions: {
     parser: 'babel-eslint'
